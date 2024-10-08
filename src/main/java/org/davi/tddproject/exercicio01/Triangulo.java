1ce02628429b52a471b83fb72e382c46a2747274
Davign10 <davigusmao10@bol.com.br>
2024-10-07 22:04:30 -0300

    TDD do triangulo

--- src/main/java/org/davi/tddproject/exercicio01/Triangulo.java
+++ src/main/java/org/davi/tddproject/exercicio01/Triangulo.java
@@ -1,4 +1,49 @@
 package org.davi.tddproject.exercicio01;
 
 public class Triangulo {
+
+    private int lado1;
+    private int lado2;
+    private int lado3;
+
+    public String verificaTriangulo(int lado1, int lado2, int lado3) {
+        if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
+            return "Valores inválidos";
+        }
+        if (lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1) {
+            return "Não é um triângulo";
+        }
+        if (lado1 == lado2 && lado2 == lado3) {
+            return "Equilátero";
+        } else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+            return "Isósceles";
+        } else {
+            return "Escaleno";
+        }
+    }
+
+    public Integer getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(Integer lado1) {
+        this.lado1 = lado1;
+    }
+
+    public Integer getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(Integer lado2) {
+        this.lado2 = lado2;
+    }
+
+    public Integer getLado3() {
+        return lado3;
+    }
+
+    public void setLado3(Integer lado3) {
+        this.lado3 = lado3;
+    }
+
 }
--- src/test/java/org/davi/tddproject/teste01/TrianguloTest.java
+++ src/test/java/org/davi/tddproject/teste01/TrianguloTest.java
@@ -1,4 +1,89 @@
-package org.davi.tddproject.exercicio01;
+package org.davi.tddproject.teste01;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.davi.tddproject.exercicio01.Triangulo;
 
 public class TrianguloTest {
+
+    // 1
+    @Test
+    public void testEscalenoValido() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Escaleno", triangulo.verificaTriangulo(3, 4, 5));
+    }
+
+    // 2
+    @Test
+    public void testIsoscelesValido() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Isósceles", triangulo.verificaTriangulo(5, 5, 8));
+    }
+
+    // 3
+    @Test
+    public void testEquilateroValido() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Equilátero", triangulo.verificaTriangulo(2, 2, 2));
+    }
+
+    // 4
+    @Test
+    public void testPermutacoesIsosceles() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Isósceles", triangulo.verificaTriangulo(3, 3, 2));
+        assertEquals("Isósceles", triangulo.verificaTriangulo(3, 2, 3));
+        assertEquals("Isósceles", triangulo.verificaTriangulo(2, 3, 3));
+    }
+
+    // 5
+    @Test
+    public void testUmValorZero() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Valores inválidos", triangulo.verificaTriangulo(0, 4, 5));
+    }
+
+    // 6
+    @Test
+    public void testUmValorNegativo() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Valores inválidos", triangulo.verificaTriangulo(-1, 4, 5));
+    }
+
+    // 7
+    @Test
+    public void testSomaDoisLadosIgualAoTerceiro() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Não é um triângulo", triangulo.verificaTriangulo(5, 5, 10));
+    }
+
+    // 8
+    @Test
+    public void testPermutacoesSomaDoisLadosIgualAoTerceiro() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Não é um triângulo", triangulo.verificaTriangulo(5, 10, 5));
+        assertEquals("Não é um triângulo", triangulo.verificaTriangulo(10, 5, 5));
+    }
+
+    // 9
+    @Test
+    public void testSomaDoisLadosMenorQueTerceiro() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Não é um triângulo", triangulo.verificaTriangulo(3, 4, 8));
+    }
+
+    // 10
+    @Test
+    public void testPermutacoesSomaDoisLadosMenorQueTerceiro() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Não é um triângulo", triangulo.verificaTriangulo(4, 8, 3));
+        assertEquals("Não é um triângulo", triangulo.verificaTriangulo(8, 3, 4));
+    }
+
+    // 11
+    @Test
+    public void testTresValoresZero() {
+        Triangulo triangulo = new Triangulo();
+        assertEquals("Valores inválidos", triangulo.verificaTriangulo(0, 0, 0));
+    }
 }
